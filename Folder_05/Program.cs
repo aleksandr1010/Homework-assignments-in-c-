7206d46f41fd03509afae9102efe2961bd6797ae
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-10 18:14:17 +0300

    Task_05

---
+++ Folder_05/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показать числа от -N до N
+Console.WriteLine("Введите ограничение выводимых чисел");
+int n= int.Parse(Console.ReadLine ( ));
+int a= -n;
+while(a <= n)
+{
+    Console.Write( a + " ") ;
+    a ++;
+}
