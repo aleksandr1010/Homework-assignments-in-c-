c91b3ca618d3b1e3371a489c38d362f3fb8a0a2a
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-10 18:29:21 +0300

    Task_07

---
+++ Folder_07/Program.cs
@@ -0,0 +1,11 @@
+﻿//Удалить вторую цифру трёхзначного числа
+int n = int.Parse(Console.ReadLine());
+if(n >= 100 && n < 1000)
+{
+    int last = n % 10;
+    int first = n/100;
+    int result = first * 10 + last;
+    Console.WriteLine("Number with deleted second digit: " + result);
+}
+else 
+Console.WriteLine("This is not 3 digit number");
