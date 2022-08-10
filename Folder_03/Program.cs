acdbee31bcca98a9ff9c8e03f2a940a536fcc14f
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-10 18:01:50 +0300

    Task_003

---
+++ Folder_03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Выяснить является ли число чётным
+Console.WriteLine("Введите число");
+int n;
+n=int.Parse(Console.ReadLine());
+if (n % 2==0)
+{
+Console.WriteLine("Введенное число является четным");
+}
+else
+{
+Console.WriteLine("Введенное число является не четным");
+}
+Console.Read();
