59d8cf2efd17206dd236f8f8ca42f5d7165c957c
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-10 18:22:53 +0300

    Task_06

---
+++ Folder_06/Program.cs
@@ -0,0 +1,6 @@
+﻿//Показать последнюю цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+int a = number % 10;
+Console.WriteLine("Число: " + number);
+Console.WriteLine("Единицы: " + a);
+int b = (number % 100 - number % 10) / 10;   
