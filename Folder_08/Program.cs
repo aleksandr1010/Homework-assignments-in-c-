d0aa2252938b9530cd2578cedb0e5bac9b8d8431
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-10 18:37:51 +0300

    Task_08

---
+++ Folder_08/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int a, b, c;
+ Console.WriteLine("Кратно ли число?");
+Console.WriteLine("Кратность какому числу проверяем?");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+b = int.Parse(Console.ReadLine());
+if ( b % a == 0)
+ {
+     Console.WriteLine("Число " + b + " - кратно " + a );
+ }
+    else
+ {
+    Console.WriteLine("Число " + b + " - не кратно " + a );
+ }
