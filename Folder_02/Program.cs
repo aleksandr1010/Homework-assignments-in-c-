8fc173c7b3876ed3a9b7fa000d7fc5945e0cda1f
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-10 17:55:05 +0300

    Task_002

---
+++ Folder_02/Program.cs
@@ -0,0 +1,19 @@
+﻿//Найти максимальное из трех чисел
+int a = new Random().Next(1,100);
+Console.WriteLine(a);
+int b = new Random().Next(1,100);
+Console.WriteLine(b);
+int c = new Random().Next(1,100);
+Console.WriteLine(c);
+if(a>b && a>c)
+{
+Console.WriteLine("a");
+}
+ if(b>c && b>a)
+ {
+Console.WriteLine("b");
+ }
+ if(c>b && c>a)
+ {
+Console.WriteLine("c");
+ }
