1411f94155765670ed8aa34501b63d387ddd062e
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-10 17:40:56 +0300

    Task_01

---
+++ Foler_01/Program.cs
@@ -0,0 +1,32 @@
+﻿//По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите число от 1 до 7");
+int n = int.Parse(Console.ReadLine());
+
+if (n==1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (n==2)
+{
+    Console.WriteLine("Вторник");
+}
+if (n==3)
+{
+    Console.WriteLine("Среда");
+}
+if (n==4)
+{
+    Console.WriteLine("Четверг");
+}
+if (n==5)
+{
+    Console.WriteLine("Пятница");
+}
+if (n==6)
+{
+    Console.WriteLine("Суббота");
+}
+if (n==7)
+{
+    Console.WriteLine("Воскресенье");
+}
