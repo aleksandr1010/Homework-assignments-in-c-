69a9ad1ec5dc88f56c224c5e99361b22d43b62c4
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-10 18:08:14 +0300

    Task_004

---
+++ Folder_04/Program.cs
@@ -0,0 +1,6 @@
+﻿//Показать вторую цифру трёхзначного числа
+int number = new Random().Next(100, 1000);
+int a = number % 10;
+Console.WriteLine("Число: " + number);
+int b = (number % 100 - number % 10) / 10;
+Console.WriteLine("Десятки: " + b);
